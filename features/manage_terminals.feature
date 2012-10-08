8604ded48b9c8bd64b9963933dc67bf76e4dd167
Andrei Lisnic <alisnic@nebulent.com>
2012-10-08 16:27:50 +0300

    Add new scenarios and unit tests for terminals

--- features/manage_terminals.feature
+++ features/manage_terminals.feature
@@ -7,3 +7,14 @@ Feature: Manage terminals
   Scenario: Add terminal
     Given I add a terminal
     Then The terminal should be in the list
+
+  Scenario: Delete a terminal
+    Given I have some random terminals
+      And I am on the terminals page
+    When I delete one of the terminals
+    Then The terminal should not be in the list
+
+  Scenario: Edit a terminal
+    Given I have some random terminals
+      And I edit on of them
+     Then The updated terminal should be in the list
--- spec/controllers/terminals_controller_spec.rb
+++ spec/controllers/terminals_controller_spec.rb
@@ -1,12 +1,29 @@
 require "spec_helper"
 
 describe TerminalsController do
-  context "index" do
-    before(:each) { get :index }
+  it "should have a list of all terminals" do
+    get :index
+    terminals = Terminal.all
+    assigns(:terminals).should == terminals
+  end
 
-    it "should have terminals as instance var" do
-      terminals = Terminal.all
-      assigns(:terminals).should == terminals
+  it "should have a new terminal" do
+    get :new
+    assigns(:terminal).should be_a_new_record
+  end
+
+  context "validation errors" do
+    xit "should show them when creating a terminal" do
+
+    end
+
+    xit "should show them when editing a terinal" do
+
     end
   end
+
+  xit "should be able to delete a terminal" do
+
+  end
+
 end
