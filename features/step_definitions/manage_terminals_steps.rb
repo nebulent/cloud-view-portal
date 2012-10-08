d07dc079aea77b029ac32486ae6a810a5fbfcea4
Andrei Lisnic <alisnic@nebulent.com>
2012-10-08 15:28:30 +0300

    Avoid cucumber warnings

--- features/step_definitions/manage_terminals_steps.rb
+++ features/step_definitions/manage_terminals_steps.rb
@@ -1,13 +1,11 @@
 Given "I add a terminal" do
-  steps %Q{
-    Given I am on the terminals page
-    And I click "Add terminal"
-    And I fill in the following:
-      | terminal_name  | t1                            |
-      | terminal_uri   | vnc://alisnic@localhost:9292  |
-    And I press "Add terminal"
-    Then I should be on terminals page
-  }
+  step 'I am on the terminals page'
+  step 'I click "Add terminal"'
+  step 'I fill in the following:', table(%{
+    | terminal_name  | t1                            |
+    | terminal_uri   | vnc://alisnic@localhost:9292  |})
+  step 'I press "Add terminal"'
+  step 'I should be on terminals page'
 end
 
 Then /^I should see it in terminals list$/ do
