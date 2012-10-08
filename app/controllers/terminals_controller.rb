b6cb02f7a91ecdeb5c6414b65fa51294bdd8daf4
Andrei Lisnic <alisnic@nebulent.com>
2012-10-08 15:19:15 +0300

    add required methods

--- app/controllers/terminals_controller.rb
+++ app/controllers/terminals_controller.rb
@@ -2,4 +2,16 @@ class TerminalsController < ApplicationController
   def index
     @terminals = Terminal.all
   end
+
+  def new
+    @terminal = Terminal.new
+  end
+
+  def create
+    @terminal = Terminal.new(params[:terminal])
+
+    if @terminal.save
+      redirect_to terminals_path
+    end
+  end
 end
