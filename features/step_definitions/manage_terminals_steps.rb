a24805c55a0fecc91c00add21d72e07f09ce5692
Andrei Lisnic <alisnic@nebulent.com>
2012-10-09 00:38:59 +0300

    Use a factory when adding a terminal

--- features/step_definitions/manage_terminals_steps.rb
+++ features/step_definitions/manage_terminals_steps.rb
@@ -1,15 +1,12 @@
 Given "I add a terminal" do
-  @terminal_name = SecureRandom.hex(10)
-  login = SecureRandom.hex(5)
-  host = SecureRandom.hex(8)
-  @terminal_uri = "vnc://#{login}@#{host}:9292"
+  @added_terminal = FactoryGirl.create :terminal
 
   steps %Q{
     Given I am on the terminals page
       And I click "Add terminal"
       And I fill in the following:
-        | terminal_name  | #{@terminal_name}  |
-        | terminal_uri   | #{@terminal_uri}   |
+        | terminal_name  | #{@added_terminal.name}  |
+        | terminal_uri   | #{@added_terminal.uri}   |
       And I press "Add terminal"
     Then I should be on terminals page
   }
@@ -18,8 +15,8 @@ end
 Then "The terminal should be in the list" do
   steps %Q{
     Given I am on the terminals page
-    Then I should see "#{@terminal_name}"
-     And I should see "#{@terminal_uri}"
+    Then I should see "#{@added_terminal.name}"
+     And I should see "#{@added_terminal.uri}"
   }
 end
 
