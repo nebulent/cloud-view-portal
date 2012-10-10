736a5869cece2323dbcb0f27e3ad1e7014db1de5
Andrei Lisnic <alisnic@nebulent.com>
2012-10-10 11:45:21 +0300

    Add some methods to terminal model

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -5,4 +5,16 @@ class Terminal < ActiveRecord::Base
   validates_presence_of :name, :uri
   validates_uniqueness_of :name, :uri
 
+  def username
+    URI(uri).user
+  end
+
+  def host
+    URI(uri).host
+  end
+
+  def port
+    URI(uri).port
+  end
+
 end
--- spec/models/terminal_spec.rb
+++ spec/models/terminal_spec.rb
@@ -5,6 +5,24 @@ describe Terminal do
     FactoryGirl.build(:terminal).should be_valid
   end
 
+  it 'should get the the username from uri' do
+    rand_user = SecureRandom.hex(5)
+    terminal = FactoryGirl.build :terminal, uri: "vnc://#{rand_user}@host.com"
+    terminal.username.should == rand_user
+  end
+
+  it 'should get the host from the uri' do
+    rand_host = SecureRandom.hex(6) + '.com'
+    terminal = FactoryGirl.build :terminal, uri: "vnc://jora@#{rand_host}:7777"
+    terminal.host.should == rand_host
+  end
+
+  it 'should get the port from the uri' do
+    rand_port = (rand * 10000).to_i
+    terminal = FactoryGirl.build :terminal, uri: "vnc://jora@host.com:#{rand_port}"
+    terminal.port.should == rand_port
+  end
+
   describe "validations" do
     before (:each) { FactoryGirl.create :terminal }
 
