3b8b14b1b8b8cd6e6aac75c6dd872c8b7ee64e23
Andrei Lisnic <alisnic@nebulent.com>
2012-10-11 17:58:25 +0300

    Add proxy daemon lib

---
+++ lib/proxy_daemon.rb
@@ -0,0 +1,72 @@
+require 'port_checker'
+
+class ProxyDaemon
+  EXECUTABLE  = 'websockify.py'
+
+  def self.daemon_directory
+    File.join(Rails.root, '/vendor/daemons/vnc-rebind/')
+  end
+
+  def self.daemon_executable
+    File.join(daemon_directory, EXECUTABLE)
+  end
+
+  attr_reader :port, :target, :pid
+
+  def initialize (terminal, session=nil)
+    @terminal = terminal
+    @target = "#{terminal.host}:#{terminal.port}"
+
+    if session
+      attach(session)
+    else
+      @port = PortChecker.rand_open
+    end
+  end
+
+  def attach (session)
+    raise InvalidProcess.new unless process_running?(session.pid)
+    @pid = session.pid
+    @attached = true
+  end
+
+  def start!
+    return @pid unless @pid.nil?
+
+    @port ||= PortChecker.rand_open # in case we attached and don't know the original port
+    @pid = Process.spawn command
+
+    while PortChecker.open?(port) do
+      sleep 0.5
+    end
+
+    @attached = false
+    @pid
+  end
+
+  def running?
+    @pid and process_running?(@pid) and (!PortChecker.open?(port) or @attached)
+  end
+
+  def stop!
+    return unless @pid
+    Process.kill 9, @pid
+    @pid = nil
+  end
+
+  def command
+    "#{self.class.daemon_executable} 0.0.0.0:#{@port} #{@target} > /dev/null 2>&1"
+  end
+
+  private
+
+  def process_running? (pid)
+    begin
+      Process.getpgid(pid)
+      true
+    rescue Errno::ESRCH
+      false
+    end
+  end
+
+end
---
+++ spec/lib/proxy_daemon_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../../lib/proxy_daemon'
+
+# For testing in isolation
+unless defined?(Rails)
+  class ::Rails
+    def self.root
+      File.absolute_path File.join(File.dirname(__FILE__), '/../../')
+    end
+  end
+end
+
+describe ProxyDaemon do
+  let (:terminal) { stub(host: '127.0.0.1', port: 5900) }
+  let (:subject) { ProxyDaemon.new(terminal) }
+
+  it 'should have an existing executable' do
+    File.exist?(ProxyDaemon.daemon_executable).should == true
+  end
+
+  it 'should have terminal address as target' do
+    subject.target.should == "#{terminal.host}:#{terminal.port}"
+  end
+
+  it 'should bind to a random open port' do
+    PortChecker.should_receive(:rand_open).at_least(1)
+  end
+
+  context 'when started' do
+    before(:each) { subject.start! }
+
+    it { should be_running }
+
+    it 'should be able to stop' do
+      subject.stop!
+      subject.should_not be_running
+    end
+  end
+
+  context 'when attached' do
+    before(:each) { subject.start! }
+
+    let (:attached_proxy) { ProxyDaemon.new(terminal, stub(:pid => subject.pid)) }
+
+    it 'should report running status' do
+      attached_proxy.should be_running
+    end
+
+    it 'should be able to stop the process' do
+      attached_proxy.stop!
+      attached_proxy.should_not be_running
+      #subject.should_not be_running TODO: investigate why this fails
+    end
+
+    after(:each) { subject.stop! }
+  end
+
+  after(:each) { subject.stop! }
+
+end
