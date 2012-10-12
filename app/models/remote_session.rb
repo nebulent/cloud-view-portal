d4fd710e9f4a4f5634226a56e96b88a29738b93b
Andrei Lisnic <alisnic@nebulent.com>
2012-10-12 12:26:23 +0300

    Add host and port to remote session

--- app/models/remote_session.rb
+++ app/models/remote_session.rb
@@ -2,7 +2,7 @@ require 'proxy_daemon'
 
 class RemoteSession < ActiveRecord::Base
 
-  attr_accessible :pid, :terminal_id
+  attr_accessible :pid, :terminal_id, :host, :port
 
   belongs_to :terminal
   validates_presence_of :terminal
@@ -11,7 +11,10 @@ class RemoteSession < ActiveRecord::Base
   before_destroy :stop_proxy
 
   def launch_proxy
-    self.pid = ProxyDaemon.new(terminal).start!
+    daemon = ProxyDaemon.new(terminal)
+    self.pid = daemon.start!
+    self.host = daemon.host
+    self.port = daemon.port
   end
 
   def stop_proxy
--- spec/models/remote_session_spec.rb
+++ spec/models/remote_session_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe RemoteSession do
+
   before(:all)   { @terminal = FactoryGirl.create :terminal }
   let (:subject) { RemoteSession.new(terminal_id: @terminal.id) }
 
@@ -8,18 +9,24 @@ describe RemoteSession do
   it { should validate_presence_of(:terminal) }
 
   context 'proxy daemon' do
-    let (:daemon) { stub(:start! => 20121, :stop! => nil) }
+    let (:daemon) { stub(start!: 20121, stop!: nil, host: '127.0.0.1', port: 21212) }
     before(:each) { ProxyDaemon.stub!(:new).and_return(daemon) }
+    before(:each) { subject.save! }
 
     it 'should launch a proxy upon creation' do
-      daemon.should_receive(:start!)
-      subject.save!
       subject.pid.should == 20121
+    end
+
+    it 'should assign proxy host to self' do
+      subject.host.should == daemon.host
+    end
+
+    it 'should assign proxy port to self' do
+      subject.port.should == daemon.port
     end
 
     it 'should kill the proxy when destroyed' do
       daemon.should_receive(:stop!)
-      subject.save!
       subject.destroy
     end
   end
