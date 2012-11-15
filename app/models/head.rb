69a65fa787c3f23f6c7b40f4206170299c90e7c2
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-15 16:45:01 +0200

    Organize views

--- app/models/head.rb
+++ app/models/head.rb
@@ -2,9 +2,9 @@ class Head < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  accepts_nested_attributes_for :organization
-
   has_one :organization
+
+  accepts_nested_attributes_for :organization
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
 end
