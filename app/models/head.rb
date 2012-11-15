388a6a4dcdd4dd538e34daa45ddec92d9910a44c
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-15 15:24:13 +0200

    Add organization to head

--- app/models/head.rb
+++ app/models/head.rb
@@ -2,5 +2,7 @@ class Head < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :organization
+
   attr_accessible :email, :password, :password_confirmation, :remember_me
 end
