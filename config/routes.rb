e10b41fb33554ce7286fd7d7e12c73d7efe4f901
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-15 14:51:18 +0200

    Add pages controller for static stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cloudview::Application.routes.draw do
+  match '/pages/:action', :controller => "pages", :as => "pages"
+
+  root to: 'pages', action: 'index'
+
   resources :terminals
   resources :remote_sessions, only: [:create, :destroy]
 end
