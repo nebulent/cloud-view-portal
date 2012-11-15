475d646191e13f9fa366dac3fd5f7154e50304a3
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-15 16:11:18 +0200

    head accepts attributes for organization

--- app/models/head.rb
+++ app/models/head.rb
@@ -2,6 +2,8 @@ class Head < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  accepts_nested_attributes_for :organization
+
   has_one :organization
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
