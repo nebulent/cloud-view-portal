35c8ee6d05f4d397823bd43d058e812c95ddf731
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-15 15:13:47 +0200

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121012073040) do
+ActiveRecord::Schema.define(:version => 20121115131005) do
+
+  create_table "heads", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "heads", ["email"], :name => "index_heads_on_email", :unique => true
+  add_index "heads", ["reset_password_token"], :name => "index_heads_on_reset_password_token", :unique => true
 
   create_table "remote_sessions", :force => true do |t|
     t.integer "terminal_id"
