d25e2953e1cdf79b3b96e6089060e3b54485b9fd
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-16 16:12:29 +0200

    Implement create action

--- app/controllers/organizations/connections_controller.rb
+++ app/controllers/organizations/connections_controller.rb
@@ -1,11 +1,43 @@
 class Organizations::ConnectionsController < Organizations::ApplicationController
   before_filter :fetch_terminal
+  before_filter :get_protocols, :only => [:edit, :new]
 
   def index
     @connections = @terminal.connections
   end
 
+  def new
+    @connection = @terminal.connections.new
+  end
+
+  def create
+    @connection  = @terminal.connections.new(params[:connection])
+
+    if @connection.save
+      flash.now[:success] = 'The connection has been successfully created'
+      redirect_to organizations_terminal_connections_path(@terminal.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    #
+  end
+
+  def update
+    #
+  end
+
+  def destroy
+    #
+  end
+
   private
+
+  def get_protocols
+    @protocols = Connection::PROTOCOLS
+  end
 
   def fetch_terminal
     @terminal = Terminal.find(params[:terminal_id])
