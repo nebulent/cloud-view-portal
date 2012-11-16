f3a89f45bf7f58371faa9ca94f807dca4029db8f
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-16 14:50:26 +0200

    Redirect to dashboard after organization login

--- app/controllers/organizations/registrations_controller.rb
+++ app/controllers/organizations/registrations_controller.rb
@@ -1,11 +1,11 @@
 class Organizations::RegistrationsController < Devise::RegistrationsController
 
   def after_sign_in_path_for (resource)
-    organizations_terminals_path
+    organizations_dashboard_path 'index'
   end
 
   def after_sign_up_path_for (resource)
-    organizations_terminals_path
+    organizations_dashboard_path 'index'
   end
 
 end
