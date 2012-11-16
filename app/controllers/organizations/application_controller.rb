f234665d2d1d6b6fb48f44e7cdfbbb1a94ffed42
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-16 11:24:51 +0200

    Auth any organization action, fetch organization

--- app/controllers/organizations/application_controller.rb
+++ app/controllers/organizations/application_controller.rb
@@ -1,3 +1,9 @@
 class Organizations::ApplicationController < ApplicationController
+  before_filter :authenticate_head!
+  before_filter :fetch_organization
+
+  def fetch_organization
+    @organization = current_head.organization
+  end
 
 end
