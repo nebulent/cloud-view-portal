9d2fd1a5391867fde8be9058f07b0b4c6082b3a9
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-19 13:47:59 +0200

    Fix organization registration

--- app/models/head.rb
+++ app/models/head.rb
@@ -3,8 +3,10 @@ class Head < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :organization
-
   accepts_nested_attributes_for :organization
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  validates_presence_of :organization
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :organization_attributes
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,4 +6,6 @@ class Organization < ActiveRecord::Base
 
   attr_accessible :name
 
+  validates_presence_of :name
+
 end
