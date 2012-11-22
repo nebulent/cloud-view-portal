527d5ee7084c251dabe382bc80db2a0d6053af1e
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-22 11:24:24 +0200

    Add new factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :terminal do
     name { SecureRandom.hex(10) }
-    uri { "vnc://#{SecureRandom.hex(5)}@#{SecureRandom.hex(8)}:9292" }
+    uri { "#{SecureRandom.hex(5)}@#{SecureRandom.hex(8)}" }
+  end
+
+  factory :connection do
+    association :terminal
+    port { rand(64000) }
+    user { SecureRandom.hex(10) }
+    protocol { Connection::PROTOCOLS.sample }
+    credentials { SecureRandom.hex(10) }
+  end
+
+  factory :remote_session do
+    association :connection
   end
 end
