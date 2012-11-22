9c2318f5cda7ba90976de70ff7f1fc71d1b606fa
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-22 10:47:58 +0200

    Add Head tests

--- spec/models/head_spec.rb
+++ spec/models/head_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Head do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:organization) }
+  it { should accept_nested_attributes_for(:organization) }
+  it { should validate_presence_of(:organization) }
 end
