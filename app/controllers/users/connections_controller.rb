271bdad05bff1d64f6ecf751fa97c6ce43853c38
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-26 11:12:49 +0200

    Add ssh connection prototype

--- app/controllers/users/connections_controller.rb
+++ app/controllers/users/connections_controller.rb
@@ -1,7 +1,14 @@
 class Users::ConnectionsController < Users::ApplicationController
 
   def show
-    redirect_to "/clients/vnc/?uid=#{current_user.id}&sid=#{params[:id]}"
+    connection = Connection.find(params[:id])
+
+    if connection.protocol == :vnc
+      redirect_to "/clients/vnc/?uid=#{current_user.id}&sid=#{params[:id]}"
+    else
+      @ssh_relay_host = Rails.application.config.ssh_relay_host
+      @credentials = connection.to_json
+    end
   end
 
 end
