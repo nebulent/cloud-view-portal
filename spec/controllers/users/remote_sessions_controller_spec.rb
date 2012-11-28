fa172af653aca71ef2ab29013f397a534ef1f316
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-11-28 11:06:28 +0200

    Refactor sessions_controller#destroy

--- spec/controllers/users/remote_sessions_controller_spec.rb
+++ spec/controllers/users/remote_sessions_controller_spec.rb
@@ -3,37 +3,33 @@ require 'spec_helper'
 describe Users::RemoteSessionsController do
 
   let (:session_attrs) { { host: '127.0.0.1', port: 5300, id: rand(100)} }
-  let (:dummy_session) { stub(session_attrs) }
+  let (:dummy_session) { stub(session_attrs.merge(connection: stub)) }
   let (:dummy_terminal) { stub(id: rand(100), create_session: dummy_session) }
-  before(:each) { Terminal.stub(:find).and_return(dummy_terminal) }
 
   context '#create' do
-    it 'should find the terminal' do
-      Terminal.should_receive(:find).with(dummy_terminal.id.to_s)
-      post :create, term_id: dummy_terminal.id
-    end
-
-    it 'should create a session on this terminal' do
-      dummy_terminal.should_receive(:create_session)
-      post :create, term_id: dummy_terminal.id
-      JSON.parse(response.body).should == session_attrs.stringify_keys
+    it 'should delegate to ConnectionHelper' do
+      #
     end
   end
 
   context '#destroy' do
-    before (:each) do
+    let (:dummy_helper) { stub(destroy_session: rand(10)) }
+
+    before do
       RemoteSession.stub(:find).and_return(dummy_session)
-      dummy_session.stub(:destroy)
+      ConnectionSessionHelper.stub :new => dummy_helper
     end
 
-    it 'should find the requested session' do
-      RemoteSession.should_receive(:find).with(dummy_session.id.to_s)
+    it 'should delegate to ConnectionHelper' do
+      ConnectionSessionHelper.should_receive :new,
+         with: [dummy_session.connection, dummy_session]
+      dummy_helper.should_receive :destroy_session
       post :destroy, id: dummy_session.id
     end
 
-    it 'destroy the session' do
-      dummy_session.should_receive(:destroy)
+    it 'should render destroy status as json' do
       post :destroy, id: dummy_session.id
+      response.body.should == { success: dummy_helper.destroy_session }.to_json
     end
   end
 
