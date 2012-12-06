c839d5b40f32c00ba4b54e66439402f9eb11c432
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-12-06 12:52:37 +0200

    Add sample data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+org = Organization.create name: 'TestOrg'
+head = Head.create email: 'test@org.com', password: 'qweqwe',
+                   password_confirmation: 'qweqwe'
+
+user = User.create email: 'user@org.com', password: 'qweqwe',
+                   password_confirmation: 'qweqwe'
+
+org.users << user
