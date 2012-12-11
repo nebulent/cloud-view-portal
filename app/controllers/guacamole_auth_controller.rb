018b3773c8eebb214354443b0d6b8ab9ef2bfcfd
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-12-11 14:36:18 +0200

    Isolate all the logic into a domain object

--- app/controllers/guacamole_auth_controller.rb
+++ app/controllers/guacamole_auth_controller.rb
@@ -1,12 +1,11 @@
 class GuacamoleAuthController < ApplicationController
 
   def check
-    user = User.find_by_email(params[:username])
-
-    if user and user.valid_password?(params[:password])
-      return render json: GuacamoleAuthProvider.user_connections(user)
-    else
-      return render nothing: true, status: 401
+    begin
+      render json: GuacamoleAuthProvider.user_connections(params[:username],
+                                                          params[:password])
+    rescue GuacamoleAuthProvider::InvalidCredentialsError
+      render nothing: true, status: 401
     end
   end
 
