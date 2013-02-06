aec48c7016c1d492e1cb3fb329fc0e794075d080
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-06 11:47:59 +0200

    Add role to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  enum :role [:users, :admin, :account_admin]
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :role
 
   belongs_to :organization
   has_and_belongs_to_many :connections, join_table: :users_connections
