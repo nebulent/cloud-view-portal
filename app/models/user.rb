c6e5568c0c8160fd69e53706d22fb9e4b658acd8
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-06 11:52:17 +0200

    Correct roles in user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  enum :role [:users, :admin, :account_admin]
-
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :role
+         :recoverable, :rememberable, :trackable, :validatable
+
+  enum :role, [:users, :admin, :account_admin]
 
   belongs_to :organization
   has_and_belongs_to_many :connections, join_table: :users_connections
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation,
+                  :remember_me, :role
 end
