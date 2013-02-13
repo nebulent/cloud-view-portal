e0039289e27dfe3db2ea7da77ece8e4f39ac9079
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 17:29:13 +0200

    Correct the event helper

--- app/controllers/organizations/application_controller.rb
+++ app/controllers/organizations/application_controller.rb
@@ -3,7 +3,7 @@ class Organizations::ApplicationController < ApplicationController
   before_filter :authenticate_head!
   before_filter :fetch_organization
 
-  def event (attrs)
+  def event
     event_params = {:entity => self.class.name, :name => self.action_name}
     @event_logger ||= current_organization.log(event_params)
   end
