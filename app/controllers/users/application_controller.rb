01616956f416743940721f610997c98a84fdeb13
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 17:54:31 +0200

    Add event helper to users controller

--- app/controllers/users/application_controller.rb
+++ app/controllers/users/application_controller.rb
@@ -1,3 +1,8 @@
 class Users::ApplicationController < ApplicationController
   before_filter :authenticate_user!
+
+  def event
+    event_params = {:entity => self.class.name, :name => self.action_name}
+    @event_logger ||= current_user.log(event_params)
+  end
 end
