9f87d59138a00a65cc027c545e0c5ed606ad4623
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 13:01:59 +0200

    Add current_organization method to app controller

--- app/controllers/organizations/application_controller.rb
+++ app/controllers/organizations/application_controller.rb
@@ -3,8 +3,12 @@ class Organizations::ApplicationController < ApplicationController
   before_filter :authenticate_head!
   before_filter :fetch_organization
 
+  def current_organization
+    fetch_organization
+  end
+
   def fetch_organization
-    @organization = current_head.organization
+    @organization ||= current_head.organization
   end
 
 end
