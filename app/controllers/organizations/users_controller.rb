aed161514de1da554fe70c59e2338edb7d933fff
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 17:45:14 +0200

    Add events to users controller

--- app/controllers/organizations/users_controller.rb
+++ app/controllers/organizations/users_controller.rb
@@ -1,12 +1,17 @@
 class Organizations::UsersController < Devise::RegistrationsController
-
   layout 'organizations/application'
+
+  def event
+    event_params = {:entity => self.class.name, :name => self.action_name}
+    @event_logger ||= current_organization.log(event_params)
+  end
 
   def create
     build_resource(params[:user])
 
     if resource.save
       flash.now[:success] = 'The user was successfully added'
+      event.info(:message => "User with id #{resource.id} has been registered")
       return redirect_to after_sign_up_path_for(resource)
     else
       render 'new'
