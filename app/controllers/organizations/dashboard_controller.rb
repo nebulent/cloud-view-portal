e479905d9a0d77b491939a95d33e3b1349b619ce
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 13:02:22 +0200

    Respond to json requests in logs method

--- app/controllers/organizations/dashboard_controller.rb
+++ app/controllers/organizations/dashboard_controller.rb
@@ -1,2 +1,8 @@
 class Organizations::DashboardController < Organizations::ApplicationController
+  def logs
+    respond_to do |fmt|
+      fmt.html { render 'logs' }
+      fmt.json { render :json => current_organization.events.to_json }
+    end
+  end
 end
