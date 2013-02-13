10f05b698e5b6090f62c455bb37bb6e71c290d0e
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-13 15:24:03 +0200

    Add support for eq and neq.

--- app/domain/event_log_presenter.rb
+++ app/domain/event_log_presenter.rb
@@ -1,9 +1,9 @@
 class EventLogPresenter
   def self.events (org, params)
-    puts params
-    data = org.events
-    paged = page(data, params)
-    sorted = sort(paged, params)
+    data      = org.events
+    paged     = page(data, params)
+    filtered  = filter(paged, params)
+    sorted    = sort(filtered, params)
 
     {
       :data => sorted,
@@ -13,9 +13,33 @@ class EventLogPresenter
 
   private
 
+  def self.filter (data, params)
+    return data unless params["filter"]
+
+    filters = params["filter"]["filters"]
+    first = filter_criteria(data, filters["0"])
+
+    if filters.size == 1
+      first
+    else
+      logic = params["filter"]["logic"].to_sym
+      filter_criteria(first.send(logic), filters["1"])
+    end
+  end
+
+  def self.query_method (operator)
+    @query_methods ||= {"neq" => :ne, "eq" => :where}
+    @query_methods[operator]
+  end
+
+  def self.filter_criteria (data, filter)
+    key = filter["field"].to_sym
+    method = query_method(filter["operator"])
+    data.send(method, {key => filter["value"]})
+  end
+
   def self.sort (collection, params)
     return collection unless params["sort"]
-    puts "sorting..."
     field = params["sort"]["0"]["field"].to_sym
     direction = int(params["sort"]["0"]["dir"])
     collection.sort(field => direction)
