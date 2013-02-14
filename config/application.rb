9ac738e8a4c15c0fa3d1f7cae2e851f151a7bc6a
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-14 15:16:39 +0200

    Use ActiveRecord generators instead of mongoid ones

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,8 @@ module Cloudview
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.generators do |g|
+      g.orm :active_record
+    end
   end
 end
