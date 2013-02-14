389f8b808f8d20803d88f07e80582f466dc1fca4
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-14 16:54:01 +0200

    Move current_organization method to helpers

--- app/controllers/organizations/application_controller.rb
+++ app/controllers/organizations/application_controller.rb
@@ -1,19 +1,8 @@
 class Organizations::ApplicationController < ApplicationController
-
   before_filter :authenticate_head!
-  before_filter :fetch_organization
 
   def event
     event_params = {:entity => self.class.name, :name => self.action_name}
     @event_logger ||= current_organization.log(event_params)
   end
-
-  def current_organization
-    fetch_organization
-  end
-
-  def fetch_organization
-    @organization ||= current_head.organization
-  end
-
 end
