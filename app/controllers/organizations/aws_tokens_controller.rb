88d81745194fd8ba77fd833a16d2dcb8dd13fd31
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-02-28 15:28:25 +0200

    Rename references in tokens controller

--- app/controllers/organizations/aws_tokens_controller.rb
+++ app/controllers/organizations/aws_tokens_controller.rb
@@ -4,37 +4,37 @@ class Organizations::AwsTokensController < Organizations::ApplicationController
   end
 
   def new
-    @token = Token.new
+    @token = AwsToken.new
   end
 
   def create
-    @token = Token.new(params[:token])
+    @token = AwsToken.new(params[:token])
     if @token.save
       event.info(:message => "Token with ID #{@token.id} created")
-      redirect_to organizations_tokens_path
+      redirect_to organizations_aws_tokens_path
     else
       render :new
     end
   end
 
   def edit
-    @token = Token.find(params[:id])
+    @token = AwsToken.find(params[:id])
   end
 
   def update
-    @token = Token.new
+    @token = AwsToken.new
     if @token.update_attributes(params[:token])
       event.info(:message => "Token with ID #{@token.id} updated")
-      redirect_to organizations_tokens_path
+      redirect_to organizations_aws_tokens_path
     else
       render :edit
     end
   end
 
   def destroy
-    @token = Token.find(params[:id])
+    @token = AwsToken.find(params[:id])
     @token.destroy
     event.info(:message => "Token with ID #{@token.id} deleted")
-    redirect_to organizations_tokens_path
+    redirect_to organizations_aws_tokens_path
   end
 end
