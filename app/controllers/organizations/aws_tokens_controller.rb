f524aefe9723a9eb34da21ac6dd67fee202b1d27
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-03-01 15:48:24 +0200

    Allow custom actions for aws tokens

--- app/controllers/organizations/aws_tokens_controller.rb
+++ app/controllers/organizations/aws_tokens_controller.rb
@@ -5,22 +5,30 @@ class Organizations::AwsTokensController < Organizations::ApplicationController
 
   def new
     @has_aws_key = current_organization.has_amazon_credentials?
+    @actions = AwsToken::ACTIONS
     @token = AwsToken.new
   end
 
   def create
     user = User.find(params[:aws_token][:user_id])
     period = params[:aws_token][:period]
-    link = Amazon.create_access_token(current_organization, user.email, period.to_i)
 
-    @token = AwsToken.new :period => period,
-                          :link => link
-
-    if @token.save
-      event.info(:message => "Token with ID #{@token.id} created")
-      redirect_to organizations_aws_tokens_path
+    @token = AwsToken.new params[:aws_token].except(:actions)
+    if !params[:aws_token][:actions]
+      flash.now[:error] = 'you must specifiy at least one allowed action'
+      @actions = AwsToken::ACTIONS
+      @has_aws_key = current_organization.has_amazon_credentials?
+      return render('new')
     else
-      render :new
+      actions = params[:aws_token][:actions]
+      @token.link = Amazon.create_access_token(current_organization, user.email,
+                                               period.to_i, actions)
+      if @token.save
+        event.info(:message => "Token with ID #{@token.id} created")
+        redirect_to organizations_aws_tokens_path
+      else
+        render :new
+      end
     end
   end
 
