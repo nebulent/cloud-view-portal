45479ad4f093e1afe86145488b666e7447d9cc3b
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-03-01 15:49:29 +0200

    List actions in AwsToken

--- app/models/aws_token.rb
+++ app/models/aws_token.rb
@@ -1,5 +1,8 @@
 class AwsToken < ActiveRecord::Base
-  attr_accessible :link, :period
+  ACTIONS = ["cloudwatch:*", "dynamodb:*", "ec2:*", "elasticmapreduce:*",
+             "rds:*", "s3:*", "sdb:*", "sns:*", "sqs:*"]
+
+  attr_accessible :link, :period, :user_id
   belongs_to :users
 
   validates_presence_of :link, :period
