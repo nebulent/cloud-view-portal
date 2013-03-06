389241a90e0cda6bb5134d09f32e65d3900b13fc
Vladislav Ledniov <vledniov@nebulent.com>
2013-03-06 16:38:40 +0200

    Uncomment the CVP_GUAC_RELAY line

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Users::DashboardController < Users::ApplicationController
 
   def index
     @connections = current_user.connections
-    @guac_check_path = ENV['VAM_GUAC_RELAY'] + "/guacamole/configs"
+    @guac_check_path = ENV['CVP_GUAC_RELAY'] + "/guacamole/configs"
   end
 
   def show
