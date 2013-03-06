12e657f9e854c122c8dfe0e08e41dc55b9c46d35
Vladislav Ledniov <vledniov@nebulent.com>
2013-03-06 17:46:43 +0200

    Rebase

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -5,7 +5,4 @@ class Users::DashboardController < Users::ApplicationController
     @guac_check_path = ENV['CVP_GUAC_RELAY'] + "/guacamole/configs"
   end
 
-  def show
-    @user = Organization.find_by_subdomain!(request.subdomain).users.first
-  end
 end
