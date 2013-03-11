4151cb3ae6f22bf663aa9e30b4b32c6eb1ad901b
Ivan Danci <idanci@nebulent.com>
2013-03-11 17:18:58 +0200

    Hide app logic

--- app/controllers/users/connections_controller.rb
+++ app/controllers/users/connections_controller.rb
@@ -1,26 +1,26 @@
 class Users::ConnectionsController < Users::ApplicationController
 
   def show
-    connection = current_user.connections.find(params[:id])
-    terminal = connection.terminal
+    @connection = current_user.connections.find(params[:id])
+    terminal = @connection.terminal
     event.info(:message => "user connected to terminal ##{terminal.id}
-                            via connection ##{connection.id}")
+                            via connection ##{@connection.id}")
 
-    if connection.protocol == :ssh
+    if @connection.protocol == :ssh
       @ssh_relay_host = ENV['VAM_SSH_RELAY']
-      @token = SshEndpoint.create_token(connection)
+      @token = SshEndpoint.create_token(@connection)
       render 'show_ssh'
     else
       @guacamole_relay = ENV['VAM_GUAC_RELAY'] + "/guacamole"
-      @token = GuacamoleEndpoint.create_token(current_user, connection)
-      suffix = "/client.xhtml?id=#{connection.protocol}-#{connection.terminal.uri}"
+      @token = GuacamoleEndpoint.create_token(current_user, @connection)
+      suffix = "/client.xhtml?id=#{@connection.protocol}-#{@connection.terminal.uri}"
       @view_url = @guacamole_relay + suffix
       render 'show_guac'
     end
   end
 
   def leave
-    connection = current_user.connections.find(params[:id])
+    @connection = current_user.connections.find(params[:id])
     terminal = connection.terminal
     event.info(:message => "user disconnected from terminal ##{terminal.id}
                             via connection ##{connection.id}")    
