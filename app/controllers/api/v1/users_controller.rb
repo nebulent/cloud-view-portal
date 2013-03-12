aa25a2f02740f03b7b1d47012707189ad7bd8bb2
Dumitru Ursu <dima@ceata.org>
2013-03-12 23:02:44 +0200

    basic implementation of API for users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      respond_to :json
+
+      def index
+        respond_with User.all
+      end
+
+      def create
+        respond_with User.create(params[:user])
+      end
+    end
+  end
+end
